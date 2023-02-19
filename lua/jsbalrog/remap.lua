3a7a5d2813422a79cf9c0b20d29fc71ac7a4d1ed
jsbalrog <ted.jenkins@familysearch.org>
2023-02-19 16:33:43 -0700

    More fugitive fixes

--- lua/jsbalrog/remap.lua
+++ lua/jsbalrog/remap.lua
@@ -115,7 +115,7 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Fugitive --
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set('n', '<leader>gw', ':Git write<CR>')
+vim.keymap.set('n', '<leader>gw', ':Git add<CR>')
 vim.keymap.set('n', '<leader>gc', ':Git commit -v<CR>')
 vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
 vim.keymap.set('n', '<leader>gu', ':Git pull<CR>')
